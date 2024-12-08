c5018ca23d28e5abe16ec9981629af6c00f8bdb8
Kusum2613 <am5779766@gmai.com>
2024-12-08 13:13:18 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-import Mode from './Mode';
-
+import React,{useState} from 'react'
+import "./index.css"
 
 function App() {
-  return (
-    <div className="App">
-      <Mode/>
-      
-    </div>
-  );
-}
+ 
+    const [bgcolor,setBgcolor]=useState("white")
+    const [color,setColor]=useState("black")
+    const [txt,setTxt]=useState("bright")
+    function handleTheme(){
+      if (bgcolor=="white"){
+        setBgcolor("black")
+        setColor("white")
+        setTxt("Bright")
+      }else{  
+  
+        setBgcolor("white")
+        setColor("black")
+        setTxt("Dark")
+  
+      }
+    }
+    return (
+      <div className="App" style={{ backgroundColor: bgcolor, color: color }}>
+        <h1>Hello students</h1>
+        <button onClick={handleTheme}>{txt}</button>
+        
+      </div>
+    );
+  }
 
-export default App;
+
+
+
+
+ 
+
+export default App
