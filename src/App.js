eeea6c54796575150d15c0ab8e056378ed3e8206
Kusum2613 <am5779766@gmai.com>
2024-12-09 15:18:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
-import React, { useState }  from 'react'
+import React,{useState} from 'react'
 
 function App() {
-  
-  const [data,setData]=useState({
-
-   name:"",
-   email:"",
-   password:""
-
-
-
-
-
-  })
-  const[condition,setCondition]=useState(false)
-  function handleSubmit(e){
-    e.preventDefault()
-    setCondition(true)
+  const[jokee,setJokee]=useState("")
+ async function handleJoke(){
+   let data=await fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single");
+   let jokyy=await data.json()
+   setJokee(jokyy)
   }
-  
-  
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-      <input type='text' placeholder='Enter Name' value={data.name} onChange={(e)=>setData((prev)=>({...prev ,name:e.target.value}))}/><br></br>
-      <input type='email' placeholder='Enter Email'value={data.email} onChange={(e)=>setData((prev)=>({...prev,email:e.target.value}))}/><br></br>
-      <input type='password' placeholder='Enter Password'value={data.password} onChange={(e)=>setData((prev)=>({...prev,password:e.target.value}))}/><br></br>
-      <input type='Submit' value="Submit"/><br></br>
-      {condition?<h1>my name is{data.name} email is{data.email}</h1>:"plz submit"}
-      </form>
+   <h1>{jokee.joke}</h1>
+      <button onClick={handleJoke}>GenerateJoke</button>
     </div>
   )
 }
